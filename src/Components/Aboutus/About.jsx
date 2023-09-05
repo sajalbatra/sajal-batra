c7e860e536d943bc880b82751eae22b683a3ba56
Sajal Batra <batrasajal7@gmail.com>
2023-09-05 11:05:07 +0530

    About me Section added

--- src/Components/Aboutus/About.jsx
+++ src/Components/Aboutus/About.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./about.css"
+import sajalabout from "../../Styles/sajalheadshot.jpg"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about-us-page-heading'>
+    <h1>About Me</h1>
+    <div className='about-us-page'>
+      <div className='about-us-page-left'>
+      <img src={sajalabout} alt='sajal-about'/>
+      </div>
+      <div className='about-us-page-right'>
+      <h1>Bit about me</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. </p>
+      </div>
+    </div>
+    </div>
   )
 }
 
